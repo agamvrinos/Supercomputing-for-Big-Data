c20c9f898d6915fb24c121c00a27626146bc7053
P1ranhas <agg_gam@hotmail.com>
2018-09-22 19:25:15 +0200

    Update implementation and add comments
    - introduce Lift-JSON library
    - filter false-positive topics
    - sort results in descending order and keep the 10 most talked topics
    - build JSON format and export the results in a results.json file

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.1"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1"
+libraryDependencies += "net.liftweb" %% "lift-json" % "3.3.0"
--- src/main/scala/application/RDDSparkApplication.scala
+++ src/main/scala/application/RDDSparkApplication.scala
@@ -1,21 +1,34 @@
 package application
 
-import java.io.{File, InputStream, PrintWriter}
+import java.io._
 import java.text.SimpleDateFormat
 import java.util.Locale
 
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json.Serialization.write
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
+
+case class DateRecordPair(data: String, result: ArrayBuffer[Result])
+case class Result(topic: String, count: Int)
 
 object RDDSparkApplication {
   var COMMA_SPLIT = ","
   var SEMICOLON_SPLIT = ";"
   var TAB_SPLIT = "\t"
+  var IGNORED_TOPICS = Array(
+    "Type ParentCategory",
+    "CategoryType ParentCategory",
+    "Read Full",
+    "Read Full Blog"
+  )
+
   var CONFIG_FILE_PATH: String = "/local_index.txt"
 
-  var CUSTOM_DATE_FORMAT = "dd/MM/yyyy"
+  var CUSTOM_DATE_FORMAT = "yyyy-MM-dd"
   var RAW_DATE_FORMAT = "yyyyMMddHHmmss"
 
   def main(args: Array[String]): Unit = {
@@ -38,43 +51,58 @@ object RDDSparkApplication {
       val linePair = line.split(TAB_SPLIT, -1)
       val rawDate = linePair(1)
       val rawNames = linePair(23)
-
-      val formattedDate = dateFormat.format(format.parse(rawDate))
+      val formattedDate:String = dateFormat.format(format.parse(rawDate))
       (formattedDate, rawNames)
     })
 
-    val validPairRDDs = dateToStringDataRDD.filter(x => !x._2.isEmpty)   // filter out the corrupted lines
+    // filter out the corrupted lines
+    val validPairRDDs = dateToStringDataRDD.filter(x => !x._2.isEmpty)
 
+    // create (date, (topic, 1)) pairs
     val valuesSplittedRdd = validPairRDDs
       .flatMapValues(line => line.split(SEMICOLON_SPLIT, -1))
-      .mapValues(line => (line.split(COMMA_SPLIT, -1)(0), line.split(COMMA_SPLIT, -1)(1).toInt))
+      .mapValues(line => (line.split(COMMA_SPLIT, -1)(0), 1))
 
-    val groupedByDateRDDs =  valuesSplittedRdd.groupByKey()
-    val groupedByDateSummedRDDs = groupedByDateRDDs.mapValues(x => x.groupBy(_._1).mapValues(x => x.map(_._2).sum).toList)    // Single date, data tuple
+    // filter out the false positives
+    val filteredTopicsRdd = valuesSplittedRdd.filter(pair => !IGNORED_TOPICS.contains(pair._2._1))
 
-    // REMOVE ME
-    printResults(groupedByDateSummedRDDs.collect())
+    // group the (date, (topic, 1) by date
+    val groupedByDateRDDs =  filteredTopicsRdd.groupByKey()
+
+    // calculate the sum for each date and for each topic
+    val groupedByDateSummedRDDs = groupedByDateRDDs.mapValues(x =>
+      x.groupBy(_._1).mapValues(x => x.map(_._2).sum).toList)
+
+    // sort them and keep the first 10
+    val sortedRDD = groupedByDateSummedRDDs.mapValues(x => x.sortBy(x => -x._2).take(10))
+
+    val jsonResults: String = serializeResultsToJSON(sortedRDD.collect())
+    saveJSONResultsToFile(jsonResults)
 
     sc.stop()
-
   }
 
-  /**
-    * Saves the values of the "results" parameter for in "results.txt"
-    * TESTING purposes.
-    * @param results the grouped by date results
-    */
-  def printResults(results: Array[(String, List[(String, Int)])]): Unit = {
-    val pw = new PrintWriter(new File("results.txt" ))
+  private def serializeResultsToJSON(results: Array[(String, List[(String, Int)])]): String = {
+    val container = new ArrayBuffer[DateRecordPair]()
 
-    for (res <- results) {
-      var str: String =  res._1 + " \n=====================================\n"
-      for (res2 <- res._2) {
-        str += res2._1 + ": " + res2._2 + "\n"
+    for (tuple <- results) {
+      val l = new ArrayBuffer[Result]()
+      for (topicCountPair <- tuple._2) {
+        val result = Result(topicCountPair._1, topicCountPair._2)
+        l.append(result)
       }
-      pw.write(str)
-    }
 
-    pw.close
+      container.append(DateRecordPair(tuple._1, l))
+    }
+    implicit val formats = DefaultFormats
+    val jsonString = write(container)
+    jsonString
+  }
+
+  private def saveJSONResultsToFile(jsonResults: String): Unit = {
+    val file = new File("results.json")
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(jsonResults)
+    bw.close()
   }
 }
