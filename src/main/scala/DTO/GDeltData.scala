6aa6c108569a8c153dbac5040acd9419584b2e68
P1ranhas <agg_gam@hotmail.com>
2018-09-22 20:22:41 +0200

    Dataframe/Dataset Initial implementation

---
+++ src/main/scala/DTO/GDeltData.scala
@@ -0,0 +1,33 @@
+package DTO
+
+import java.sql.Timestamp
+
+case class GDeltData (
+  id: String,
+  date: Timestamp,
+  sourceCollectionId: Int,
+  sourceCommonNames: String,
+  documentIdentifier: String,
+  counts: String,
+  v2Counts: String,
+  themes: String,
+  v2Themes: String,
+  locations: String,
+  v2Locations: String,
+  persons: String,
+  v2Persons: String,
+  organizations: String,
+  v2Organizations: String,
+  v2Tone: String,
+  dates: String,
+  gCam: String,
+  sharingImages: String,
+  relatedImages: String,
+  socialImageEmbeds: String,
+  socialVideoEmbeds: String,
+  quotations: String,
+  allNames: String,
+  amounts: String,
+  translationInfo: String,
+  extras: String
+)
---
+++ src/main/scala/application/DataframeSparkApplication.scala
@@ -0,0 +1,106 @@
+package application
+
+import java.io.InputStream
+import java.text.SimpleDateFormat
+
+import DTO.GDeltData
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Dataset, RelationalGroupedDataset, SparkSession}
+
+import scala.io.Source
+
+object DataframeSparkApplication {
+
+  var COMMA_SPLIT = ","
+  var SEMICOLON_SPLIT = ";"
+  var TAB_SPLIT = "\t"
+  var IGNORED_TOPICS = Array(
+    "Type ParentCategory",
+    "CategoryType ParentCategory",
+    "Read Full",
+    "Read Full Blog"
+  )
+  var CONFIG_FILE_PATH: String = "/local_index.txt"
+  var CUSTOM_DATE_FORMAT = "yyyy-MM-dd"
+
+  def main(args: Array[String]): Unit = {
+    val dateFormat = new SimpleDateFormat(CUSTOM_DATE_FORMAT)
+
+    val schema = StructType(
+      Array(
+        StructField("id", StringType, nullable = true),
+        StructField("date", TimestampType, nullable = true),
+        StructField("sourceCollectionId", IntegerType, nullable = true),
+        StructField("sourceCommonNames", StringType, nullable = true),
+        StructField("documentIdentifier", StringType, nullable = true),
+        StructField("counts", StringType, nullable = true),
+        StructField("v2Counts", StringType, nullable = true),
+        StructField("themes", StringType, nullable = true),
+        StructField("v2Themes", StringType, nullable = true),
+        StructField("locations", StringType, nullable = true),
+        StructField("v2Locations", StringType, nullable = true),
+        StructField("persons", StringType, nullable = true),
+        StructField("v2Persons", StringType, nullable = true),
+        StructField("organizations", StringType, nullable = true),
+        StructField("v2Organizations", StringType, nullable = true),
+        StructField("v2Tone", StringType, nullable = true),
+        StructField("dates", StringType, nullable = true),
+        StructField("gCam", StringType, nullable = true),
+        StructField("sharingImages", StringType, nullable = true),
+        StructField("relatedImages", StringType, nullable = true),
+        StructField("socialImageEmbeds", StringType, nullable = true),
+        StructField("socialVideoEmbeds", StringType, nullable = true),
+        StructField("quotations", StringType, nullable = true),
+        StructField("allNames", StringType, nullable = true),
+        StructField("amounts", StringType, nullable = true),
+        StructField("translationInfo", StringType, nullable = true),
+        StructField("extras", StringType, nullable = true)
+      )
+    )
+
+    val stream: InputStream = getClass.getResourceAsStream(CONFIG_FILE_PATH)
+    val lines: List[String] = (for (line <- Source.fromInputStream( stream , "UTF16").getLines) yield line).toList
+
+    val session = SparkSession
+      .builder
+      .appName("SparkProject")
+      .master("local")
+      .getOrCreate()
+
+    import session.implicits._
+    val df = session.read
+      .schema(schema)
+      .option("sep", "\t")
+      .option("timestampFormat", "yyyyMMddS")
+      .csv(lines:_*)
+      .as[GDeltData]
+
+    // filter out the records with null values at the allNames column
+    val validData = df.filter("allNames is not null")
+
+    // keep the columns we are interested in and format the date
+    val dateAllNamesRecords: Dataset[(String, String)] = validData.map(x => (dateFormat.format(x.date), x.allNames))
+
+    // split the merged topics
+    val dateSingleTopicRecords = dateAllNamesRecords.flatMap {case (x1, x2) => x2.split(";").map((x1, _))}
+
+    // rename columns for clarity
+    var dateSingleTopicRecordsRenamed = dateSingleTopicRecords.withColumnRenamed("_1", "date")
+    dateSingleTopicRecordsRenamed = dateSingleTopicRecordsRenamed.withColumnRenamed("_2", "allNamesMerged")
+
+    val wagner = dateSingleTopicRecordsRenamed
+      .withColumn("word", split($"allNamesMerged", ",").getItem(0))
+      .withColumn("count", lit(1))
+
+    val neti= wagner.drop("allNamesMerged")
+
+    var filteredTopics = neti.filter(!col("word").isin(IGNORED_TOPICS :_*))
+
+    val aggregated = filteredTopics.groupBy("date", "word").agg(sum($"count") as "Total").toDF()
+
+    val sorted = aggregated.orderBy(desc("Total"))
+
+    sorted.show(10)
+  }
+}
